4611557e17a6cf9341bd05cd9afbbf14c7eff4a8
paddy202557 <paddyvkr@gmail.com>
2025-09-18 19:01:15 -0400

    Add files via upload

---
+++ 1_QQ_Univariate.py
@@ -0,0 +1,15 @@
+##Splitting the dataset  columns to Quantitative (Numerical) and Qualitative (String based on the data type 
+class QQUnivariate():
+    
+    def quanQual(dataset):
+        quan=[]
+        qual=[]
+        for columnName in dataset.columns:
+            #print(columnName)
+            if(dataset[columnName].dtype=='O'):
+                #print("qual")
+                qual.append(columnName)
+            else:
+                #print("quan")
+                quan.append(columnName)
+        return quan,qual
